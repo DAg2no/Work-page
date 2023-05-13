9312c95459a7414827c60f9090cfc7de35575630
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-05-13 16:06:11 -0400

    ...

--- work-page-vite/src/app.jsx
+++ work-page-vite/src/app.jsx
@@ -1,45 +1,88 @@
 import React, { useEffect } from 'react';
-import './app.css';
 import Typed from 'react-typed';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import image from './data/image.png';
+import './app.css';
+import inkendin from './data/In-Blue-34.png'
+
+ 
 
 const Header = () => {
   useEffect(() => {
     const header = document.getElementById('header');
     header.style.animation = 'slide-in 1s ease-in-out';
-    
+
     const title = document.getElementById('title');
     title.style.animation = 'bounce 2s infinite';
+    title.style.animationDuration = '2s';
   }, []);
 
   return (
-    <div className='container'>
-        <div className='header' id='header'>
-      <h1 className='title' id='title'>
-        hi, i am Daniel
+    <>
+      <div className='header-container' id='header'>
+      <h5>Daniel Uribe</h5>
+      <Container className='p-5'>
         
-      </h1>
+        <Row className='d-flex'>
+          <Col xs={7} md={6} lg={7} className='order-2 order-lg-1'>
+            <div className='title-container'>
+              <div>
+                <h1 className='title' id='title'>
+                  Hi, I am Daniel
+                </h1>
+              </div>
+            </div>
+            <span>
+              <div className='animated-typing'>
+                <Typed
+                  strings={[
+                    'Beginner in developer',
+                    'Fgrkergkm',
+                    'Rghfmn',
+                    "I'm in developer"
+                  ]}
+                  typeSpeed={40}
+                  backSpeed={30}
+                  loop
+                />
+              </div>
+            </span>
+            <div className='d-flex justify-content-center mt-3'>
+              <a href="https://www.linkedin.com/in/daniel-uribe-pulgar-93905a273/">
+                <button className='btn btn-light mx-2'>
+        <img src={inkendin} alt='button1' height='25'width='25'/>
+      </button>
+              </a>
       
-      <span>
-        <div className='animated-typing'>
-            <Typed
-              strings={[
-                "beginner in developer",
-                "fgrkergkm",
-                "rghfmn"
-              ]}
-              typeSpeed={40}
-              backSpeed={30}
-              loop                
+      <button className='btn btn-light mx-2'>
+        <img src='https://example.com/image2.png' alt='button2' height='30'width='20'/>
+      </button>
+      <button className='btn btn-light mx-2'>
+        <img src='https://example.com/image3.png' alt='button3' height='30'width='20'/>
+      </button>
+      <button className='btn btn-light mx-2'>
+        <img src='https://example.com/image4.png' alt='button4' height='30' width='20'/>
+      </button>
+    </div>
+
+          </Col>
+          <Col xs={5} md={6} lg={5} className='d-flex order-1 order-lg-2'>
+            <img
+              src={image}
+              alt='header-img'
+              className='img-fluid enlarged'
             />
-        </div>
-      </span>
-      <div>
-        <img src='src/data/image.png' alt='description' className='header-image' />
-      </div>
-      
+          </Col>
+        </Row>
+      </Container>
     </div>
-    </div>
+    <section>
+
+    </section>
+    </>
     
+
   );
 };
 
