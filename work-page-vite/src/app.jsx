594ce3014d6f3fe9013058b968bfc241869ad8c8
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-05-12 23:40:02 -0400

    ...

--- work-page-vite/src/app.jsx
+++ work-page-vite/src/app.jsx
@@ -2,32 +2,45 @@ import React, { useEffect } from 'react';
 import './app.css';
 import Typed from 'react-typed';
 
-const Header = () => { 
+const Header = () => {
   useEffect(() => {
     const header = document.getElementById('header');
     header.style.animation = 'slide-in 1s ease-in-out';
     
     const title = document.getElementById('title');
-    title.style.animation = 'pulse 2s infinite';
+    title.style.animation = 'bounce 2s infinite';
   }, []);
 
   return (
-    <div className='header' id='header'>
-      <h1 className='title' id='title'>hi, i am Daniel</h1>
+    <div className='container'>
+        <div className='header' id='header'>
+      <h1 className='title' id='title'>
+        hi, i am Daniel
+        
+      </h1>
+      
       <span>
-        <Typed>
-            strings={[
+        <div className='animated-typing'>
+            <Typed
+              strings={[
                 "beginner in developer",
                 "fgrkergkm",
                 "rghfmn"
-            ]}
-            typeSpeed={150}
-            backSpeed={100}
-            loop
-        </Typed>
+              ]}
+              typeSpeed={40}
+              backSpeed={30}
+              loop                
+            />
+        </div>
       </span>
+      <div>
+        <img src='src/data/image.png' alt='description' className='header-image' />
+      </div>
+      
     </div>
+    </div>
+    
   );
-}
+};
 
 export default Header;
