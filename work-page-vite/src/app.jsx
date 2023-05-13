8b96aab34507fe3f7dcb993c83d5026db967c34f
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-05-12 22:19:01 -0400

    ...

--- work-page-vite/src/app.jsx
+++ work-page-vite/src/app.jsx
@@ -1,11 +1,33 @@
-import 'app.css'
+import React, { useEffect } from 'react';
+import './app.css';
+import Typed from 'react-typed';
 
+const Header = () => { 
+  useEffect(() => {
+    const header = document.getElementById('header');
+    header.style.animation = 'slide-in 1s ease-in-out';
+    
+    const title = document.getElementById('title');
+    title.style.animation = 'pulse 2s infinite';
+  }, []);
 
-function App(){ 
-    return {
-        <header>
-        </header>
-    }
+  return (
+    <div className='header' id='header'>
+      <h1 className='title' id='title'>hi, i am Daniel</h1>
+      <span>
+        <Typed>
+            strings={[
+                "beginner in developer",
+                "fgrkergkm",
+                "rghfmn"
+            ]}
+            typeSpeed={150}
+            backSpeed={100}
+            loop
+        </Typed>
+      </span>
+    </div>
+  );
 }
 
-export default App;
+export default Header;
