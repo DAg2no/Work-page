ae4dfd41f7c65a64e35ed435e14bc7ab7464862a
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-06-03 17:28:16 -0400

    ...

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,30 +2,36 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Switch from "../switch";
-import '../switch/index.css'
+import "../switch/index.css";
 
 const NavBar = () => {
   return (
-    <Navbar
-      expand="md"
-      variant="light"
-      className="fixed-top bg-dark border-top-0 border-danger"
-    >
-      <Navbar.Brand>
-        <span
-          style={{ fontWeight: "normal", fontSize: "large" }}
-          className="text-light p-4"
-        >
-          Daniel Uribe
-        </span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          <Switch />
-        </Navbar.Text>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav>
+      <Navbar
+        expand="md"
+        variant="light"
+        className="fixed-top border-top-0 border-danger"
+      >
+        <Navbar.Brand>
+          <span>
+            <h2
+              style={{ fontWeight: "normal", fontSize: "large" }}
+              className="ps-3"
+            >
+              Daniel Uribe
+            </h2>
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <div className="pe-4">
+              <Switch />
+            </div>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    </nav>
   );
 };
 
