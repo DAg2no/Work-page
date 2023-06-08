bca933d2e78af5cea905b18263587e28436d83cf
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-06-08 17:13:37 -0400

    ...

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "../../components/header";
-/* import Skill from '../../components/skill'; */
+import SectionSkill from "../../components/skill"
 import "./index.css"
 
 const Home = () => {
   return (
     <>
       <Header />
-      {/* <Skill /> */}
+      <SectionSkill /> 
     </>
   );
 };
