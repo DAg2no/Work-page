f82ffa4a695ffe264c641fbfa9220200b7a51566
Daniel <92042414+DAg2no@users.noreply.github.com>
2023-07-03 20:52:11 -0400

    ...

--- src/components/project-section/index.jsx
+++ src/components/project-section/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import projects from "../../../public/projects.svg";
-import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { CardActionArea, Typography } from "@mui/material";
+import { CardActionArea, Typography, Hidden } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Chip from "@mui/material/Chip";
 import Box from "@mui/material/Box";
-
+import '../../views/home/index.css'
 
 const card = () => {};
 
@@ -17,30 +16,31 @@ const Project = () => {
     <>
       <Grid
         container
-        direction="row"
+        direction="row-reverse"
         justifyContent="center"
         alignItems="center"
         alignContent="center"
-        wrap="wrap"
+        xs={10}
       >
-        <Grid item xs={12} lg={6}>
-          <Typography variant="h2" alignItems="center">
-            Project
-          </Typography>
-          <Box sx={{ width: 22, height: 22 }} className="projectImg">
-            <img src={projects} />
-          </Box>
-        </Grid>
+        <Hidden lgDown>
+          <Grid item lg={6}>
+            <Typography variant="h2" className="text-project" textAlign="center">
+              Project
+            </Typography>
+            <img src={projects} width={500} />
+          </Grid>
+        </Hidden>
         <Grid
           container
+          xs={12}
+          lg={6}
           spacing={1}
           direction="row"
           justifyContent="center"
           alignItems="center"
           alignContent="center"
-          wrap="wrap"
         >
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} sm={6}>
             <Card sx={{ maxWidth: "100%" }}>
               <CardActionArea>
                 <CardContent>
@@ -54,7 +54,7 @@ const Project = () => {
               </CardActionArea>
             </Card>
           </Grid>
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} sm={6}>
             <Card sx={{ maxWidth: "100%" }}>
               <CardActionArea>
                 <CardContent>
@@ -68,7 +68,7 @@ const Project = () => {
               </CardActionArea>
             </Card>
           </Grid>
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} sm={6}>
             <Card sx={{ maxWidth: "100%" }}>
               <CardActionArea>
                 <CardContent>
@@ -82,7 +82,7 @@ const Project = () => {
               </CardActionArea>
             </Card>
           </Grid>
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} sm={6}>
             <Card sx={{ maxWidth: "100%" }}>
               <CardActionArea>
                 <CardContent>
